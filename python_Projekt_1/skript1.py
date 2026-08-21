b735582dc17514bcc20a5510a9a7661c47f6e1c0
Notabletoad <marvelstudios872@gmail.com>
2026-08-21 12:18:36 +0200

    Käsebrot

---
+++ python_Projekt_1/skript1.py
@@ -0,0 +1,16 @@
+def gehe_zur_schule(snack):
+
+    print('Anziehen und losgehen..')
+    print()
+    print(f'auf dem Schulweg esse ich: {snack}')
+
+if __name__ == '__main__':
+    print('schulmorgen beginnt')
+
+    mein_brot = 'Käsebrot'          #Benutzt nur das erste
+    mein_brot = 'Schokoriegel'
+    mein_brot = 'Riegel'
+    mein_brot = 'Käsebrot'
+    gehe_zur_schule(mein_brot)
+
+    print('In der Klasse hinsetzen')
