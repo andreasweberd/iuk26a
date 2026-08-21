faf3ec0d42278e864c141324b7ed6e2d9fb3e08b
Julian Firat <julianfirat@outlook.de>
2026-08-21 12:25:33 +0200

    Tung Tung Tung Sahur Update

--- python_projekt_1/script1.py
+++ python_projekt_1/script1.py
@@ -1,2 +1,10 @@
+A = "3"
 if __name__ == "__main__":
-    print("ICH BIN EIN BERLINER")
+    print("Sahur?")
+
+    A = input("Wie viele Tungs?\n")
+
+if A != "3":
+    print("FALSCH!!")
+else:
+    print("Sahur.")
