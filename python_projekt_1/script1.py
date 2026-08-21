5400da776ebec29bcf38ba66141666029cb9aa70
Julian Firat <julianfirat@outlook.de>
2026-08-21 12:32:12 +0200

    Tung Tung Tung Sahur Konsequenzen Update

--- python_projekt_1/script1.py
+++ python_projekt_1/script1.py
@@ -1,3 +1,16 @@
+def SahurStrafe():
+    schaedel = r"""
+      ______
+   .-        -.
+  /            \
+ |,  .-.  .-.  ,|
+ | )(_ /  \ _)( |
+ |/     /\     \|
+ (_     ^^     _)
+  \__|IIIIII|__/
+    """
+    print(schaedel)
+
 A = "3"
 if __name__ == "__main__":
     print("Sahur?")
@@ -6,5 +19,6 @@ if __name__ == "__main__":
 
 if A != "3":
     print("FALSCH!!")
+    SahurStrafe()
 else:
     print("Sahur.")
