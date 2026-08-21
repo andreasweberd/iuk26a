693ce689f4f248c1953a3b394f3779425d1410a3
Tom Hameier <hameier@schloss-wachenheim.de>
2026-08-21 12:18:42 +0200

    script1

---
+++ phyton_projekt_1/script1.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def gehe_zur_schule(snack: object) -> None:
+    # Use a breakpoint in the code line below to debug your script.
+    print("Anziehen und losgehen")  # Press Strg+F8 to toggle the breakpoint.
+    print(f"auf dem schulweg esse ich: {snack}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Der Schulmorgen beginnt')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mein_brot = "Käsebrot"
+    gehe_zur_schule(mein_brot)
+    print("in die Klasse setzen")
