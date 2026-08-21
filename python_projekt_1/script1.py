cbbd0c8d067355af0e4462e3ebf3ad5505eea7ff
Roos Magnus <roosmagn@bbtgruppe.net>
2026-08-21 12:19:00 +0200

    neu

---
+++ python_projekt_1/script1.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def gehe_zur_schule(snack):
+
+    print("Anziehen und losgehen...")
+    print(f"Auf dem Schulweg esse ich: {snack}  ")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Schulmorgen beginnt.")
+
+    mein_brot = "Käsebrot"
+    gehe_zur_schule(mein_brot)
+    gehe_zur_schule("MonsterEnergy")
+
+    print("In der Klasse hinsetzen.")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
