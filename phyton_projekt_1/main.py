be411ba510645f50f06eca0debd5fabc09cc070f
bouazza_karim <karim@example.com>
2026-08-21 12:29:16 +0200

    Neues_Projekt

---
+++ phyton_projekt_1/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+def gehe_zur_schule(snack):
+    print("Anziehen und Losgehen")
+    print(f"Auf dem Schulweg esse ich: {snack}")
+
+
+if __name__ == '__main__':
+    print("Schulmorgen beginnt")
+    mein_brot = "Käsebrot"
+    gehe_zur_schule(mein_brot)
+
+    print("In der Klasse hinsetzen")
