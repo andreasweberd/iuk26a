706a13b2ae858e751da610261a5bd9bb3fb88422
unknown <christopherus@example.de>
2026-08-21 12:28:51 +0200

    Neues Python projekt

---
+++ python_projekt_1/script1.py
@@ -0,0 +1,13 @@
+
+def gehe_zur_schule(snack):
+    print("Anziehen und losgehen")
+    print(f"Auf dem Schuleweg esse ich: {snack}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Schulmorgen beginnt.")
+
+    mein_brot = "Käsebrot"
+    gehe_zur_schule(mein_brot)
+
+    print("In der Klasse hinsetzen")
