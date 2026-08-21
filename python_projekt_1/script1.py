6fb57d51eef8ff2ca79aeea860361fdf31f2a686
Michelle Mayer <michelle@example.com>
2026-08-21 12:24:55 +0200

    erstes script

---
+++ python_projekt_1/script1.py
@@ -0,0 +1,12 @@
+def gehZurSchule(snack):
+    print("Anziehen und losgehen...")
+    print(f"auf dem Schulweg esse ich: {snack}")
+
+if __name__ == '__main__':
+    print('Schulmorgen beginnt.')
+
+    meinBrot= "Käsebrot"
+    gehZurSchule(meinBrot)
+
+    print("In Klasse hinzetzen")
+
