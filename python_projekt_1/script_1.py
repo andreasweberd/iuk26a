e817904fb30b09ac8e0a1e2fff436715d2247663
wechter_jannis-josua <wechter_jannis-josua@example.com>
2026-08-22 19:13:16 +0200

    Weiterführende Informationen hinzugefügt

--- python_projekt_1/script_1.py
+++ python_projekt_1/script_1.py
@@ -1,11 +1,13 @@
+# --- FUNKTIONSDEFINITION (Wartet nur, wird noch NICHT ausgeführt!) ---
 def gehe_zur_schule(snack):
     print("Anziehen und losgehen...")
     print(f"Auf dem Schulweg esse ich {snack}.")
 
+# --- EINSPRUNGPUNKT (Hier startet das Programm wirklich!) ---
 if __name__ == "__main__":
     print("Schulmorgen beginnt.")
 
     mein_brot = "Käsebrot"
-    gehe_zur_schule(mein_brot)
+    gehe_zur_schule(mein_brot)  # Funktionsaufruf mit Parameter
 
     print("In der Klasse hinsetzen.")
