8fcb104f6e2d405dc877b0410248bbbf90927197
Roos Magnus <roosmagn@bbtgruppe.net>
2026-09-04 09:09:52 +0200

    abgeben

---
+++ 2026_09_4/kaufen.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def kaufen(artikel, preis, anzahl):
+    # Use a breakpoint in the code line below to debug your script.
+    gesamtpreis = anzahl * preis
+    print(f"Ich kaufe {anzahl} * {artikel} für insgesamt {gesamtpreis} Euro")  # Press Strg+F8 to toggle the breakpoint.
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    anzahl = 3
+    preis = 4.5
+    artikelname = "Schokolade"
+    kaufen(artikelname, preis, anzahl)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
