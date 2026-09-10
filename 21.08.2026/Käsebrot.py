4817ec3583b383bafd63691aa7320f4bac722308
Notabletoad <marvelstudios872@gmail.com>
2026-09-10 16:27:48 +0200

    Käsebrot verschoben

---
+++ 21.08.2026/Käsebrot.py
@@ -0,0 +1,17 @@
+def gehe_zur_schule(snack):
+
+    print('Anziehen und losgehen..')
+    print()
+    print(f'auf dem Schulweg esse ich: {snack}')
+
+if __name__ == '__main__':
+    print('schulmorgen beginnt')
+
+    mein_brot = 'Wurstbrot'          #Benutzt immer das letze was in der Funktion aktuell ist 
+    mein_brot = 'Schokoriegel'
+    mein_brot3 = 'Riegel'
+    mein_brot2 = 'Käsebrot'
+    gehe_zur_schule(mein_brot)
+
+    print('In der Klasse hinsetzen')
+     
