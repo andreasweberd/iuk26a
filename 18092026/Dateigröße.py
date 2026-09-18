9a8e7f2286c121cd4410ffee2357ccc1505bff3e
Sven Gilbert <sven@gilbert-home.de>
2026-09-18 12:18:29 +0200

    Video Funktion hinzugefügt

--- 18092026/Dateigröße.py
+++ 18092026/Dateigröße.py
@@ -17,8 +17,8 @@ def berechne_video(breite, hoehe, farbtiefe, fps, abtastrate, bittiefe, kanaele,
     videospur_bits = berechne_bild(breite, hoehe, farbtiefe) * fps * zeit_in_sekunden
     audiospur_bits = berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden)
     video_bits = videospur_bits + audiospur_bits
-    video_MiB = video_bits / 8 / 1024 / 1024
-    print(f"Die Videogröße beträgt {video_MiB} MiB.")
+    video_GiB = video_bits / 8 / 1024 / 1024 / 1024
+    print(f"Die Videogröße beträgt {video_GiB} GiB.")
 
 if __name__ == "__main__":
     berechne_video(1920, 1080, 24, 30, 48000, 16, 2, 60)
