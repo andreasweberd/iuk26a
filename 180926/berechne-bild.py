da1f3f9bd36771a92ce320fc4b385cbd2ab8f307
mahsa-arbabi <mahsaarbabi221@gmail.com>
2026-09-18 12:51:16 +0200

    aufgabe 3

--- 180926/berechne-bild.py
+++ 180926/berechne-bild.py
@@ -1,10 +1,32 @@
-def berechne_bild(breite,hoehe,farbtiefe):
-    Bits=breite*hoehe*farbtiefe
-    Bytes=Bits/8
-    Kibibytes=Bytes/1024
-    Mebibytes=Kibibytes/1024
+def berechne_bild(breite, hoehe, farbtiefe):
+    Bits = breite * hoehe * farbtiefe
+    Bytes = Bits / 8
+    Kibibytes = Bytes / 1024
+    Mebibytes = Kibibytes / 1024
     return Mebibytes
 
-if __name__=="__main__":
-    Ergebnis=berechne_bild(1025,680,16)
+
+def berechne_audio(abtastrate, bittiefe, kanalae, zeit_in_sekunden):
+    Bits = abtastrate * bittiefe * kanalae * zeit_in_sekunden
+    Bytes = Bits / 8
+    Kibibytes = Bytes / 1024
+    Mebibytes = Kibibytes / 1024
+    return Mebibytes
+
+
+
+if __name__ == "__main__":
+    Ergebnis = berechne_bild(1025, 680, 16)
     print("Das Ergebnis ist:" + str(Ergebnis))
+
+    Audio_Bits = berechne_audio(44100, 16, 2, 10)
+    print("Die Audiodatei ist:" + str(Audio_Bits))
+
+def berechne_video(bild_meB,audio_meB, fps, zeit_in_sekunden):
+    mebibyte = bild_meB * fps * zeit_in_sekunden + audio_meB
+    return(mebibyte)
+
+if __name__ == "__main__":
+    video_miB = (berechne_video(berechne_bild(1920, 1080, 24), berechne_audio(48000, 16, 2, 60), 30, 60))
+    video_Gib=video_miB /1024
+    print(f"Die Größe des Videos b-eträgt: {video_miB:.2f} MiB")
