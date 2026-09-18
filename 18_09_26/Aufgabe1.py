5a121b20d66ebeaef3928763474649962e4a2d8a
Michelle Mayer <michelle@example.com>
2026-09-18 11:43:33 +0200

    Datengroeße audio programm hinzugefuegt

--- 18_09_26/Aufgabe1.py
+++ 18_09_26/Aufgabe1.py
@@ -3,5 +3,13 @@ def berechne_bild(breite, hoehe, farbtife):
     dateigroeße_Mebibyte = dateigroeße_Bit / (8 * 1024 * 1024)
     print(f"die Dateigröße beträgt {dateigroeße_Mebibyte} Mebibyte")
 
+def berechne_audio (abtastrate, bittiefe, kanaele, zeit):
+    audio_Bit = abtastrate * bittiefe * kanaele * zeit
+    audio_Mebibyte = audio_Bit / (8 * 1024 * 1024)
+    return audio_Mebibyte
+
 if __name__ == "__main__":
-    berechne_bild(1025, 680, 16)
+    bild = berechne_bild(1025, 680, 16)
+    audio = berechne_audio(44100,16,2,10)
+
+    print(f"die Dateigröße Audio beträgt {audio} Mebibyte")
