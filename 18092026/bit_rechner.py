dc0ffce26a3ce5e660f4d4d15845f79362252d32
David Heinrich <david.gesuino@icloud.com>
2026-09-18 12:07:15 +0200

    .

--- 18092026/bit_rechner.py
+++ 18092026/bit_rechner.py
@@ -24,11 +24,11 @@ def berechne_video(breite,hoehe,farbtiefe,fps,abtastrate,bittiefe,kanaele,zeit_i
 
 if __name__ == "__main__":
     
-   # dateigroesse_audio = berechne_audio(44100,16,2,10)
-   # dateigroesse_bild = berechne_bild(1600,680,16)
+    dateigroesse_audio = berechne_audio(44100,16,2,10)
+    dateigroesse_bild = berechne_bild(1600,680,16)
 
-   # print(f"Die Dateigroesse des Bildes betraegt: {dateigroesse_bild} MiB")
-   # print(f"Die Dateigroesse der Audio betraegt: {dateigroesse_audio} MiB")
+    print(f"Die Dateigroesse des Bildes betraegt: {dateigroesse_bild} MiB")
+    print(f"Die Dateigroesse der Audio betraegt: {dateigroesse_audio} MiB")
 
     dateigroesse_video = berechne_video (1920,1080,24,30,48000,16,2,60)
     print(f"Die Dateigroesse des Videos betraegt: {dateigroesse_video} MiB")
