102aa2492b79e370feb9e24b80647c8e80f7d2e8
Sven Gilbert <sven@gilbert-home.de>
2026-09-18 12:31:07 +0200

    Video Funktion hinzugefügt

--- 18092026/Dateigröße.py
+++ 18092026/Dateigröße.py
@@ -2,14 +2,12 @@
 def berechne_bild(breite, hoehe, farbtiefe):
     bild_bits = breite * hoehe * farbtiefe
     bild_MiB = bild_bits / 8 / 1024 / 1024
-    print(f"Die Bildgröße beträgt {bild_MiB} MiB.")
     return bild_bits
 
 #Funktion Aufgabe 2
 def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
     audio_bits = abtastrate * bittiefe * kanaele * zeit_in_sekunden
     audio_MiB = audio_bits / 8 / 1024 / 1024
-    print(f"Die Audiogröße beträgt {audio_MiB} MiB.")
     return audio_bits
 
 #Funktion Aufgabe 3
