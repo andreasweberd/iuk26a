669f3e26696968f4077ee72b5f5d876054fd8d08
unknown <christopherus@example.de>
2026-09-18 09:04:27 +0200

    Upload

---
+++ 180926/Aufgabe.py
@@ -0,0 +1,10 @@
+def berechne_bild(breite, hoehe, farbtiefe):
+    Bits = breite*hoehe*farbtiefe
+    Bytes = Bits/8
+    Kibibytes = Bytes/1024
+    Mebibytes = Kibibytes/1024
+    return Mebibytes
+
+if __name__ == "__main__":
+    Ergebnis _berechne_bild(1025, 680, 16)
+    print(f"Das Ergebnis ist: {Ergebnis_berechne_bild} MiB")
