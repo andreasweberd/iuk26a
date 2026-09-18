04227ab600880f88bcaab45a9803fbfc6cec7792
Tim Monzel <tim@example.com>
2026-09-18 11:38:41 +0200

    Aufgabe 2

--- 18_09_26/Aufgabe1.py
+++ 18_09_26/Aufgabe1.py
@@ -4,6 +4,14 @@ def berechne_bild(breite, hoehe, farbtiefe):
     mebibyte = bit / (8 * 1024 * 1024)
     print(f"Es sind {mebibyte} Mebibytes")
 
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
+    bit = abtastrate * bittiefe * kanaele * zeit_in_sekunden
+    mebibyte = bit / (8 * 1024 * 1024)
+    print(f"Es sind {mebibyte} Mebibytes")
 
 if __name__ == "__main__":
     berechne_bild(1920, 1080, 24)
+    berechne_audio(44100, 16, 2, 10)
+
+
+
