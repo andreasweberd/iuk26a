871fcf7342b9f807bf15477a394cbdbed9cca986
wechter_jannis-josua <wechter_jannis-josua@example.com>
2026-09-18 09:14:27 +0200

    berechne_bild fertig

--- 18.09.26/berechne_bild.py
+++ 18.09.26/berechne_bild.py
@@ -1,1 +1,10 @@
-berechne_bild(breite, höhe, farbtiefe):
+def berechne_bild(Breite, Höhe, Farbtiefe):
+    Bits = Breite*Höhe*Farbtiefe
+    Bytes = Bits/8
+    Kibibytes = Bytes/1024
+    Mebibytes = Kibibytes/1024
+    return Mebibytes
+
+if __name__ == "__main__":
+    Dateigröße = berechne_bild(1025,680,16)
+    print("Die Dateigröße ist: " + str(Dateigröße), "Mebibytes.")
