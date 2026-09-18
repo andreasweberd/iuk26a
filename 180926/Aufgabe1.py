776a0d6ad14875278e50e5efe52258cabb5fd460
erschens.luke <hallo@hallo.com>
2026-09-18 11:43:52 +0200

    a2

--- 180926/Aufgabe1.py
+++ 180926/Aufgabe1.py
@@ -8,6 +8,16 @@ def berechne_bild(breite, hoehe, farbtiefe):
 
     print(f"Das Ergebnis ist {dateigröße}")
 
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
+
+
+    audio_bits = abtastrate * bittiefe * kanaele * zeit_in_sekunden
+
+    audio_bits = audio_bits / 8
+    audio_bits = audio_bits / 1024
+    audio_bits = audio_bits / 1024
+
+    print(f"Das Ergebnis ist {audio_bits}")
 
 if __name__ == "__main__":
 
@@ -17,3 +27,9 @@ if __name__ == "__main__":
 
     berechne_bild(breite, hoehe, farbtiefe)
 
+    abtastrate = 44100
+    bittiefe = 16
+    kanaele = 2
+    zeit_in_sekunden = 10
+
+    berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden)
