165ae68780d3aa08910ed62920036769132754c7
Julian Firat <julianfirat@outlook.de>
2026-09-18 11:54:03 +0200

    Aufgabe 3

--- 180926/Aufgabe1Und2.py
+++ 180926/Aufgabe1Und2.py
@@ -12,8 +12,17 @@ def berechne_audio(abtastrate,bittiefe,kanaele,zeit_in_sekunden):
     AudioMebibytes = AudioKibibytes/1024
     return AudioMebibytes
 
+def berechne_video(breite,hoehe,farbtiefe,fps,abtastrate,bittiefe,kanaele,zeit_in_sekunden):
+    AudioData = berechne_audio(abtastrate,bittiefe,kanaele,zeit_in_sekunden)
+    FrameData = berechne_bild(breite,hoehe,farbtiefe)
+    VideoData = FrameData*fps*zeit_in_sekunden
+    Gibibytes = (VideoData+AudioData)/1024
+    return Gibibytes
+
 if __name__ == "__main__":
     Ergebnis = berechne_bild(1025,680,16)
-    print("Das Bildergebnis ist: " + str(Ergebnis))
+    print("Das Bildergebnis ist: " + str(Ergebnis) + " MiB")
     AudioErgebnis = berechne_audio(44100,16,2,10)
-    print("Das Audioergebnis ist: " + str(AudioErgebnis))
+    print("Das Audioergebnis ist: " + str(AudioErgebnis) + " MiB")
+    VideoErgebnis = berechne_video(1920,1080,24,30,48000,16,2,60)
+    print("Das Videoergebnis ist: " + str(VideoErgebnis) + " GiB")
