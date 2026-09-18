e52a0a5688f3b22607fe9cb007ad5eab49ddee7e
wechter_jannis-josua <wechter_jannis-josua@example.com>
2026-09-18 09:23:07 +0200

    Aufgabe fertig gestellt

--- 18.09.26/berechne_bild.py
+++ 18.09.26/berechne_bild.py
@@ -7,8 +7,8 @@ def berechne_bild(Breite, Höhe, Farbtiefe):
 
 if __name__ == "__main__":
     Dateigröße = berechne_bild(1025,680,16)
-    print("Die Dateigröße ist: " + str(Dateigröße), "MiB.")
-    
+    print("Die Datei ist " + str(Dateigröße), "MiB groß.")
+
 # Pixelanzahl: 1025 × 680 = 697.000 Pixel
 # Bits: 697.000 × 16 = 11.152.000 Bits
 # Bytes: 11.152.000 / 8 = 1.394.000 Bytes
