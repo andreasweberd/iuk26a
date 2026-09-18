e628f71477f1a294e01a33051caa8ac367829bd5
Marino <salvatore.marino@koehl-mb.eu>
2026-09-18 08:56:51 +0200

    Abgegeben

---
+++ 18092026/Aufgabe1.py
@@ -0,0 +1,10 @@
+def berchene_bild(breite, hoehe, farbtiefe):
+    Bits = breite * hoehe * farbtiefe
+    Bytes = Bits / 8
+    Kibibytes = Bytes / 1024
+    Mebibytes = Kibibytes / 1024
+    return Mebibytes
+
+if __name__ == "__main__":
+    Ergebnis = berchene_bild(1025,680,16)
+    print("Das Ergebnis ist: " + str(Ergebnis))
