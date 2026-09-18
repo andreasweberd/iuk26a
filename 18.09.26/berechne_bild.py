45001c3949a11ee26d5d2a51c8b99d6f1e9a2a08
wechter_jannis-josua <wechter_jannis-josua@example.com>
2026-09-18 11:46:56 +0200

    Aufgabe 2 fertig gestellt

--- 18.09.26/berechne_bild.py
+++ 18.09.26/berechne_bild.py
@@ -5,9 +5,20 @@ def berechne_bild(Breite, Höhe, Farbtiefe):
     Mebibytes = Kibibytes/1024
     return Mebibytes
 
+def berechne_audio(Abtastrate, Bittiefe, Kanäle, Zeit_in_Sekunden):
+    Bits = Abtastrate*Bittiefe*Kanäle*Zeit_in_Sekunden
+    Bytes = Bits/8
+    Kibibytes = Bytes/1024
+    Mebibytes = Kibibytes/1024
+    return Mebibytes
+
+
 if __name__ == "__main__":
     Dateigröße = berechne_bild(1025,680,16)
-    print("Die Datei ist " + str(Dateigröße), "MiB groß.")
+    print("Das Bild ist " + str(Dateigröße), "MiB groß.")
+
+    Audio_Bits = berechne_audio(44100,16,2,10)
+    print("Die Audiodatei ist " + str(Audio_Bits), "MiB groß.")
 
 # Pixelanzahl: 1025 × 680 = 697.000 Pixel
 # Bits: 697.000 × 16 = 11.152.000 Bits
