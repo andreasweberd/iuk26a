ebbaff8e1d08672d10e1af2aa9dd50a16a8ad7b8
wechter_jannis-josua <wechter_jannis-josua@example.com>
2026-09-18 12:40:56 +0200

    Aufgabe 3 fertiggestellt

--- 18.09.26/Mebibytes_berechnen.py
+++ 18.09.26/Mebibytes_berechnen.py
@@ -12,16 +12,21 @@ def berechne_audio(Abtastrate, Bittiefe, Kanäle, Zeit_in_Sekunden):
     Mebibytes = Kibibytes/1024
     return Mebibytes
 
+def berechne_video(Breite, Höhe, Farbtiefe, FPS, Abtastrate, Bittiefe, Kanäle, Zeit_in_Sekunden):
+    Bild = berechne_bild(Breite, Höhe, Farbtiefe)   
+    Audio = berechne_audio(Abtastrate, Bittiefe, Kanäle, Zeit_in_Sekunden)
+    Video = Bild*FPS*Zeit_in_Sekunden+Audio
+    Gigabyte = Video/1024
+    return Gigabyte
+
+
 
 if __name__ == "__main__":
-    Dateigröße = berechne_bild(1025,680,16)
-    print("Das Bild ist " + str(Dateigröße), "MiB groß.")
+    Bild_Bits = berechne_bild(1025,680,16)
+    print("Das Bild ist " + str(Bild_Bits), "MiB groß.")
 
     Audio_Bits = berechne_audio(44100,16,2,10)
     print("Die Audiodatei ist " + str(Audio_Bits), "MiB groß.")
 
-# Pixelanzahl: 1025 × 680 = 697.000 Pixel
-# Bits: 697.000 × 16 = 11.152.000 Bits
-# Bytes: 11.152.000 / 8 = 1.394.000 Bytes
-# Kibibytes (KiB): 1.394.000 / 1024 = 1.361,328125 KiB
-# Mebibytes (MiB): 1.361,328125 / 1024 = ~1,33 MiB
+    Video_Bits = berechne_video(1920,1080,24,30,48000,16,2,60)
+    print("Das Video ist " + str(Video_Bits), "GiB groß.")
