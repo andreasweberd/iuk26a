cc5da3de2c8d39a541881858f534000fe0ea26a0
David Heinrich <david.gesuino@icloud.com>
2026-09-18 11:43:26 +0200

    bit_audio_rechner

--- 18092026/bit_rechner.py
+++ 18092026/bit_rechner.py
@@ -1,12 +1,19 @@
 def berechne_bild(breite,hoehe,farbtiefe):
 
     bitdateigroesse = (breite*hoehe)*farbtiefe
-
     dateigroesse = ((bitdateigroesse / 8) / 1024) / 1024
+
+    return dateigroesse
+
+def berechne_audio(abtastrate,bittiefe,kanaele,zeit_in_sekunden):
+
+    audiobits = (abtastrate*bittiefe*kanaele*zeit_in_sekunden)
+    dateigroesse = ((audiobits / 8) / 1024) / 1024
 
     return dateigroesse
 
 if __name__ == "__main__":
     
-    dateigroesse = berechne_bild(1025,680,16)
+    dateigroesse = berechne_audio(44100,16,2,10)
+    # dateigroesse = berechne_bild(1600,680,16)
     print(f"Die Dateigroesse betraegt: {dateigroesse} MiB")
